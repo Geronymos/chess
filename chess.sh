0b49a0fcb197fc2c1589f907f2924b4dd0917efa
Geronymos <spielerbereich@gmail.com>
2021-07-08 20:34:03 +0200

    + captured figures list

--- chess.sh
+++ chess.sh
@@ -4,19 +4,10 @@ board=()
 for i in {0..63}; do board[$i]=" "; done;
 
 rowname="ABCDEFGH"
+captured_white=()
+captured_black=()
 
-# ♔
-# ♕
-# ♖
-# ♗
-# ♘
-# ♙
-# ♚
-# ♛
-# ♜
-# ♝
-# ♞
-# ♟
+# ♔♕♖♗♘♙♚♛♜♝♞♟
 
 for i in {8..15}; do board[$i]="b"; done;
 for i in 0 7; do board[$i]="t"; done;
@@ -29,8 +20,13 @@ for i in {48..55}; do board[$i]="B"; done;
 for i in 56 63; do board[$i]="T"; done;
 for i in 57 62; do board[$i]="H"; done;
 for i in 58 61; do board[$i]="N"; done;
-board[60]="Q"
-board[59]="K"
+board[59]="Q"
+board[60]="K"
+
+function decode_figures() {
+    sed "s/b/♙/; s/h/♘/; s/n/♗/; s/t/♖/; s/q/♔/; s/k/♕/; s/B/♟/; s/H/♞/; s/N/♝/; s/T/♜/; s/Q/♚/; s/K/♛/;" /dev/stdin
+}
+
 function render() {
     echo {A..H} | sed "s/^/  /" 
     for field in ${!board[@]}
@@ -38,8 +34,7 @@ function render() {
         # printf "$((((field) % 8 == 0)) && echo ${rowname:(((field+1)/8)):1]})"
         ((field %8 == 0)) && printf "$(((field)/8+1)) "
         printf "\e[31;4$(( ( field + (field / 8 %2 == 0) ) % 2 *7))m"
-        printf "${board[$field]} " | 
-        sed "s/b/♙/; s/h/♘/; s/n/♗/; s/t/♖/; s/q/♔/; s/k/♕/; s/B/♟/; s/H/♞/; s/N/♝/; s/T/♜/; s/Q/♚/; s/K/♛/;"
+        printf "${board[$field]} " | decode_figures
         (((field+1) % 8 == 0)) && printf "\e[0:0m\n"
         
     done
@@ -61,7 +56,11 @@ do
     to_x=${to_x:0:1}
     to_y=${code:4:1}
     echo "$from_x $from_y $to_x $to_y"
+    [[ ${board[(((to_y-1)*8-1+to_x+1))]} =~ [a-z] ]] && captured_white+=${board[(((to_y-1)*8-1+to_x+1))]}
+    [[ ${board[(((to_y-1)*8-1+to_x+1))]} =~ [A-Z] ]] && captured_black+=${board[(((to_y-1)*8-1+to_x+1))]}
     board[(((to_y-1)*8-1+to_x+1))]=${board[(((from_y-1)*8-1+from_x+1))]}
     board[(((from_y-1)*8-1+from_x+1))]=" "
+    echo ${captured_black[@]} | decode_figures
+    echo ${captured_white[@]} | decode_figures
 done
 
